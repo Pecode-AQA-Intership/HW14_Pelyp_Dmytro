891afe71e0caed5d44814e7ce94935a1ae0b9b56
Dmytro Pelyp <pelup19992@gmail.com>
2022-02-14 13:45:12 +0200

    locators changes

--- cypress/integration/hw_15.js
+++ cypress/integration/hw_15.js
@@ -37,7 +37,7 @@ it('Filling data in the "Department" field and verification', () => {
 });
 
 it('Submitting data', () => {
-    cy.get('#submit').click()
+    cy.get(DATA_LOCATORS.submit).click()
 });
 
 it('Checking the correctness of all data', () => {
@@ -98,7 +98,7 @@ it('Editing data in the "Department" field and verification', () => {
 });
 
 it('Submitting data', () => {
-    cy.get('#submit').click()
+    cy.get(DATA_LOCATORS.submit).click()
 });
 });
 
@@ -116,96 +116,96 @@ describe('Checking searching feature', () => {
 
 it('Filling data in the "First Name" field  and verification', () => {
     cy.contains('Add').click()
-    cy.get('#firstName').type(data.RANDOM_NAME)
+    cy.get(DATA_LOCATORS.firstName).type(data.RANDOM_NAME)
     .should("have.value", data.RANDOM_NAME);
 });
     
 it('Filling data in the "Last Name field" and verification', () => {
-    cy.get('#lastName').type(data.RANDOM_LAST_NAME)
+    cy.get(DATA_LOCATORS.lastName).type(data.RANDOM_LAST_NAME)
     .should("have.value", data.RANDOM_LAST_NAME);
 });
     
 it('Filling data in the "E-mail" field and verification', () => {
-    cy.get('#userEmail').type(data.RANDOM_EMAIL)
+    cy.get(DATA_LOCATORS.userEmail).type(data.RANDOM_EMAIL)
     .should("have.value", data.RANDOM_EMAIL);
 })
 
 it('Filling data in the "Age" field and verification', () => {
-    cy.get('#age').type(data.RANDOM_AGE)
+    cy.get(DATA_LOCATORS.userAge).type(data.RANDOM_AGE)
     .should("have.value", data.RANDOM_AGE);
 });
     
 it('Filling data in the "Salary" field and verification', () => {
-    cy.get('#salary').type(data.RANDOM_NUM)
+    cy.get(DATA_LOCATORS.salary).type(data.RANDOM_NUM)
     .should("have.value", data.RANDOM_NUM);
 });
     
 it('Filling data in the "Department" field and verification', () => {
-    cy.get('#department').type(data.RANDOM_ADDRESS)
+    cy.get(DATA_LOCATORS.department).type(data.RANDOM_ADDRESS)
     .should("have.value", data.RANDOM_ADDRESS);
 });
 
 it('Submitting data', () => {
-    cy.get('#submit').click()
+    cy.get(DATA_LOCATORS.submit).click()
 });
 
 it ('Clicking on searchbox field and search user by First Name', () => {
-    cy.get('#searchBox').type(data.RANDOM_NAME)
+    cy.get(DATA_LOCATORS.searchBox).type(data.RANDOM_NAME)
     .should("have.value", data.RANDOM_NAME);
 });
 
 it ('Deleting First Name value from searchbox', () => {
-    cy.get('#searchBox').click()
+    cy.get(DATA_LOCATORS.searchBox).click()
     .clear()
 });
 
 it ('Clicking on searchbox field and search user by LAST Name', () => {
-    cy.get('#searchBox').type(data.RANDOM_LAST_NAME)
+    cy.get(DATA_LOCATORS.searchBox).type(data.RANDOM_LAST_NAME)
     .should("have.value", data.RANDOM_LAST_NAME);
 });
 
 it ('Deleting Last Name value from searchbox', () => {
-    cy.get('#searchBox').click()
+    cy.get(DATA_LOCATORS.searchBox).click()
     .clear()
 });
 
 it ('Clicking on searchbox field and search user by E-Mail', () => {
-    cy.get('#searchBox').type(data.RANDOM_EMAIL)
+    cy.get(DATA_LOCATORS.searchBox).type(data.RANDOM_EMAIL)
     .should("have.value", data.RANDOM_EMAIL);
 });
 
 it ('Deleting E-mail value from searchbox', () => {
-    cy.get('#searchBox').click()
+    cy.get(DATA_LOCATORS.searchBox).click()
     .clear()
 });
 
 it ('Clicking on searchbox field and search user by Age', () => {
-    cy.get('#searchBox').type(data.RANDOM_AGE)
+    cy.get(DATA_LOCATORS.searchBox).type(data.RANDOM_AGE)
     .should("have.value", data.RANDOM_AGE);
 });
 
 it ('Deleting Age value from searchbox', () => {
-    cy.get('#searchBox').click()
+    cy.get(DATA_LOCATORS.searchBox).click()
     .clear()
 });
 
 it ('Clicking on searchbox field and search user by Salary', () => {
-    cy.get('#searchBox').type(data.RANDOM_NUM)
+    cy.get(DATA_LOCATORS.searchBox).type(data.RANDOM_NUM)
     .should("have.value", data.RANDOM_NUM);
 });
 
 it ('Deleting Salary value from searchbox', () => {
-    cy.get('#searchBox').click()
+    cy.get(DATA_LOCATORS.searchBox).click()
     .clear()
 });
 
 it ('Clicking on searchbox field and search user by Department', () => {
-    cy.get('#searchBox').type(data.RANDOM_ADDRESS)
+    cy.get(DATA_LOCATORS.searchBox).type(data.RANDOM_ADDRESS)
     .should("have.value", data.RANDOM_ADDRESS);
 });
 
 it ('Deleting Department value from searchbox', () => {
-    cy.get('#searchBox').click()
+    cy.get(DATA_LOCATORS.searchBox).click()
     .clear()
 })
 });
